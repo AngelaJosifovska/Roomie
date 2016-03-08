b916043edbb5f8a7e6e9ca6e1f170d0039fa0918
VJan-fin <vik>
2016-03-08 19:45:28 +0100

    Profile image showing on comments

--- src/main/webapp/static/app/app.js
+++ src/main/webapp/static/app/app.js
@@ -251,3 +251,30 @@ roomie.directive('fileData', [function () {
         }
     }
 }]);
+
+/**
+ * Angular directive for resizing images
+ * Use: image-resize image-percent="50"
+ */
+roomie.directive("imageResize", [
+	"$parse", function($parse) {
+		return {
+			link: function(scope, elm, attrs) {
+				var imagePercent;
+				imagePercent = $parse(attrs.imagePercent)(scope);
+				elm.bind("load", function(e) {
+					elm.unbind("load"); //Hack to ensure load is called only once
+					var canvas, ctx, neededHeight, neededWidth;
+					neededHeight = elm[0].naturalHeight * imagePercent / 100;
+					neededWidth = elm[0].naturalWidth * imagePercent / 100;
+					canvas = document.createElement("canvas");
+					canvas.width = neededWidth;
+					canvas.height = neededHeight;
+					ctx = canvas.getContext("2d");
+					ctx.drawImage(elm[0], 0, 0, neededWidth, neededHeight);
+					elm.attr('src', canvas.toDataURL("image/jpeg"));
+				});
+			}
+		};
+	}
+]); 
